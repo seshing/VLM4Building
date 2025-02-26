35c2f8b1193a9505e09a338c959e18d158b5469f
Xiucheng <81627877+seshing@users.noreply.github.com>
2025-02-26 21:09:24 +0800

    Update 05_internvl_predict_final.py

--- code/05_internvl_predict_final.py
+++ code/05_internvl_predict_final.py
@@ -12,7 +12,7 @@ from torchvision.transforms.functional import InterpolationMode
 from tqdm import tqdm
 import multiprocessing
 
-def process_images(gpu_id, image_files, model_name, image_directory, output_directory, city): 
+def process_images(gpu_id, image_files, model_name, image_directory, output_directory, city, area): 
     # Load model and tokenizer
     # path = os.path.join('/public/home/zhaotianhong/xiucheng/models/InternVL/', model_name)
     path = model_name
@@ -32,7 +32,7 @@ def process_images(gpu_id, image_files, model_name, image_directory, output_dire
     output_file1 = os.path.join(output_directory, f'responses1_gpu{gpu_id}.json')   
     output_file2 = os.path.join(output_directory, f'responses2_gpu{gpu_id}.json')
     
-    for image_name in tqdm(image_files, desc=f"Processing images for city {} on GPU {gpu_id}"):
+    for image_name in tqdm(image_files, desc=f"Processing images for {city}_{area} on GPU {gpu_id}"):
         full_image_path = os.path.join(image_directory, image_name)
         pixel_values = load_image(full_image_path, max_num=12)
         
@@ -73,49 +73,52 @@ def main():
     
     model_name = args.model
     
-    cities = ['Berlin_center', 'Manila_center', 'Berlin_outer', 'Manila_outer']
+    cities = ['Berlin']
+    areas = ['center', 'outer']
     for city in cities: 
-        image_directory = f'VLM4Building/img/{city}'
-        model_output_name = model_name.split('/')[-1]
-        output_directory = f'VLM4Building/img/{city}'
-        os.makedirs(output_directory, exist_ok=True)
+        for area in areas:
+            image_directory = f'VLM4Building/img/{city}'
+            model_output_name = model_name.split('/')[-1]
+        
+            output_directory = f'VLM4Building/img/{city}/{area}'
+            os.makedirs(output_directory, exist_ok=True)
 
-        test_df = pd.read_csv(f'VLM4Building/data/selected_img_id/{city}.csv')
-        test_id = test_df['image_name'].tolist()
-        
-        num_gpus = args.num_gpus
-        all_image_files = [img for img in os.listdir(image_directory) if img.endswith((".jpg", ".jpeg", ".png"))]
-        all_image_files = [img for img in all_image_files if img in test_id]
-        
-        # Check for already processed images
-        processed_images = set()
-        for gpu_id in range(0, num_gpus):
-            output_file = os.path.join(output_directory, f'responses1_gpu{gpu_id}.json')
-            if os.path.exists(output_file):
-                with open(output_file, 'r') as f:
-                    processed_images.update(json.load(f).keys())
-        
-        # Filter out already processed images
-        remaining_images = [img for img in all_image_files if img not in processed_images]
-        
-        # Split the remaining images into two groups
-        images_per_gpu = len(remaining_images) // num_gpus
-        image_files_split = [remaining_images[i:i + images_per_gpu] for i in range(0, len(remaining_images), images_per_gpu)]
-        
-        if len(image_files_split) > num_gpus:
-            image_files_split[num_gpus-1].extend(image_files_split[num_gpus])
-            image_files_split = image_files_split[:num_gpus]
+            test_df = pd.read_csv(f'VLM4Building/data/selected_img_id/{city}_{area}.csv')
+            test_id = test_df['image_name'].tolist()
+            
+            num_gpus = args.num_gpus
+            all_image_files = [img for img in os.listdir(image_directory) if img.endswith((".jpg", ".jpeg", ".png"))]
+            all_image_files = [img for img in all_image_files if img in test_id]
+            
+            # Check for already processed images
+            processed_images = set()
+            for gpu_id in range(0, num_gpus):
+                output_file = os.path.join(output_directory, f'responses1_gpu{gpu_id}.json')
+                if os.path.exists(output_file):
+                    with open(output_file, 'r') as f:
+                        processed_images.update(json.load(f).keys())
+            
+            # Filter out already processed images
+            remaining_images = [img for img in all_image_files if img not in processed_images]
+            
+            # Split the remaining images into two groups
+            images_per_gpu = len(remaining_images) // num_gpus
+            image_files_split = [remaining_images[i:i + images_per_gpu] for i in range(0, len(remaining_images), images_per_gpu)]
+            
+            if len(image_files_split) > num_gpus:
+                image_files_split[num_gpus-1].extend(image_files_split[num_gpus])
+                image_files_split = image_files_split[:num_gpus]
 
-        # Create and start processes
-        processes = []
-        for i in range(num_gpus):
-            p = multiprocessing.Process(target=process_images, args=(i, image_files_split[i], model_name, image_directory, output_directory, city))
-            processes.append(p)
-            p.start()
+            # Create and start processes
+            processes = []
+            for i in range(num_gpus):
+                p = multiprocessing.Process(target=process_images, args=(i, image_files_split[i], model_name, image_directory, output_directory, city, area))
+                processes.append(p)
+                p.start()
 
-        # Wait for all processes to finish
-        for p in processes:
-            p.join()
+            # Wait for all processes to finish
+            for p in processes:
+                p.join()
 
     print("All processes completed.")
 
